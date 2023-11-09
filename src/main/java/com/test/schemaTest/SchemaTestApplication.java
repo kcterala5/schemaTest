87952d62a333671d0afbcf560666adbc0406ed72
kcterala5 <kc.terala@fintheon.com>
2023-11-09 15:06:39 +0530

    Intial Commit

---
+++ src/main/java/com/test/schemaTest/SchemaTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.schemaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/schemaTest/SchemaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.schemaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
