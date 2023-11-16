a5841c5bebb58f8ec19e29ea1202e4be17c09df3
lshan0 <113054866+lshan0@users.noreply.github.com>
2023-11-16 22:50:01 +0530

    Add new scenario where a bank user can get percentile for every customer

--- src/main/java/com/test/schemaTest/service/BankService.java
+++ src/main/java/com/test/schemaTest/service/BankService.java
@@ -2,6 +2,7 @@ package com.test.schemaTest.service;
 
 import com.test.schemaTest.models.Bank;
 import com.test.schemaTest.models.BankCustomers;
+import com.test.schemaTest.models.Company;
 import com.test.schemaTest.models.CompanyData;
 import com.test.schemaTest.repository.BankCustomersRepository;
 import com.test.schemaTest.repository.BankRepository;
@@ -9,9 +10,14 @@ import com.test.schemaTest.repository.CompanyDataRepository;
 import com.test.schemaTest.utils.CompanyDataUtils;
 import com.test.schemaTest.views.CompanyDataView;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class BankService {
@@ -19,14 +25,17 @@ public class BankService {
     private final CompanyDataRepository companyDataRepository;
     private final BankRepository bankRepository;
     private final BankCustomersRepository bankCustomersRepository;
+    private final CompanyDataService companyDataService;
     private static final CompanyDataUtils companyDataUtils = new CompanyDataUtils();
 
     public BankService(final CompanyDataRepository companyDataRepository,
                        final BankRepository bankRepository,
-                       final BankCustomersRepository bankCustomersRepository) {
+                       final BankCustomersRepository bankCustomersRepository,
+                       final CompanyDataService companyDataService) {
         this.companyDataRepository = companyDataRepository;
         this.bankRepository = bankRepository;
         this.bankCustomersRepository = bankCustomersRepository;
+        this.companyDataService = companyDataService;
     }
 
     public List<CompanyData> generateCreditRatingScoreForBank(final int bankId) {
@@ -43,9 +52,29 @@ public class BankService {
         return companyDataList;
     }
 
-    public List<CompanyDataView> getCreditRatingForBank(final int bankId) {
+    public Map<String, List<CompanyDataView>> getCreditRatingForBankCustomers(final int bankId) {
         Bank bank = bankRepository.findBankById(bankId);
-        List<CompanyDataView> companyDataList = new ArrayList<>();
-        return companyDataList;
+        List<BankCustomers> bankCustomersList = bankCustomersRepository.findBankCustomersByBankId(bankId);
+        List<CompanyData> companyDataList = bankCustomersList.stream()
+                .map(bankCustomer -> companyDataRepository.findCompanyDataByHashId(DigestUtils.md5DigestAsHex(bankCustomer.getCompany().getPanNumber().getBytes(StandardCharsets.UTF_8))))
+                .toList();
+
+        Map<String, List<CompanyData>> categorizedData = new HashMap<>();
+        for (CompanyData companyData : companyDataList) {
+            String industryType = companyData.getIndustryType();
+            String annualSales = companyData.getAnnualSales();
+
+            String key = industryType + "_" + annualSales;
+            List<CompanyData> dataList = categorizedData.computeIfAbsent(key, k -> new ArrayList<>());
+            dataList.add(companyData);
+        }
+        Map<String, List<CompanyDataView>> companyDataViewMap = new HashMap<>();
+        for (Map.Entry<String, List<CompanyData>> entry : categorizedData.entrySet()) {
+            String key = entry.getKey();
+            List<CompanyData> companyDataListForKey = entry.getValue();
+            List<CompanyDataView> companyDataViewList = companyDataService.getBankCustomersDataView(companyDataListForKey);
+            companyDataViewMap.put(key, companyDataViewList);
+        }
+        return companyDataViewMap;
     }
 }
