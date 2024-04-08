0acaecd826a1ad62639d6d9fd7b72ad2332cf2cc
kcterala <kcterala@gmail.com>
2024-04-08 12:20:29 +0530

    Refactor SQL query

--- src/main/java/com/test/schemaTest/controllers/CompanyController.java
+++ src/main/java/com/test/schemaTest/controllers/CompanyController.java
@@ -1,19 +1,23 @@
 package com.test.schemaTest.controllers;
 
+import com.test.schemaTest.repository.CompanyDataRepository;
 import com.test.schemaTest.service.CompanyDataService;
 import com.test.schemaTest.views.CompanyDataView;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CompanyController {
 
     private final CompanyDataService companyDataService;
+    private final CompanyDataRepository companyDataRepository;
 
-    public CompanyController(final CompanyDataService companyDataService) {
+    public CompanyController(final CompanyDataService companyDataService, final CompanyDataRepository companyDataRepository) {
         this.companyDataService = companyDataService;
+        this.companyDataRepository = companyDataRepository;
     }
 
     @GetMapping("/company/{panNumber}")
@@ -27,5 +31,11 @@ public class CompanyController {
     public ResponseEntity<CompanyDataView> getCompanyDataViewFromSQL(@PathVariable String panNumber) {
         CompanyDataView companyDataView = companyDataService.getCompanyDataViewFromSQL(panNumber);
         return ResponseEntity.ok().body(companyDataView);
+    }
+
+    @GetMapping("/company/add/{annual_sales}/{industry_type}")
+    public ResponseEntity<Void> updateMappings(@PathVariable String annual_sales, @PathVariable String industry_type) {
+        companyDataRepository.updateScoreMapBySalesAndIndustryType(annual_sales, industry_type);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/test/schemaTest/models/CreditRating.java
+++
@@ -1,5 +0,0 @@
-package com.test.schemaTest.models;
-
-public class CreditRating {
-
-}
